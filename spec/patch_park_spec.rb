b181062636af2a2bc5fa7e0a4ca8456ffa63a4a1
Peter Wells <petethebeatwells@gmail.com>
2020-01-31 14:20:10 -0800

    adds specs for patching a park

---
+++ spec/patch_park_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'pry'
+
+describe "patch a park route", :type => :request do
+  park_id = 0;
+  before do
+    post '/parks', params: { :name => 'Da Best Park', :state => 'Alaska', :national => true }
+    park_id = JSON.parse(response.body).fetch("id")
+  end
+
+  it 'updates the parks name' do
+    patch "/parks/#{park_id}", params: { :name => 'Yosemite'}
+    get "/parks/#{park_id}"
+    expect(JSON.parse(response.body)['name']).to eq('Yosemite')
+  end
+
+  it 'returns a successful update message' do
+    patch "/parks/#{park_id}", params: { :name => 'Yosemite II'}
+    expect(JSON.parse(response.body)['message']).to eq("This park has been updated successfully.")
+  end
+  it 'returns an exception if park does not exist' do
+    patch "/parks/#{"oooo"}", params: { :name => 'Yosemite II'}
+    expect(JSON.parse(response.body)['message']).to eq("Couldn't find Park with 'id'=oooo")
+  end
+end
--- spec/post_park_spec.rb
+++ spec/post_park_spec.rb
@@ -22,3 +22,37 @@ describe "post a park route", :type => :request do
     expect(response).to have_http_status(:created)
   end
 end
+
+
+describe "post a park with no national status", :type => :request do
+
+  before do
+      post '/parks', params: { :name => 'Da Best Park Again', :state => 'Hawaii'}
+  end
+
+  it 'returns the park with no national' do
+    expect(JSON.parse(response.body)['name']).to eq('Da Best Park Again')
+  end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
+end
+
+
+# describe "bad post call", :type => :request do
+#
+#   it 'returns an exception if name is blank' do
+#     post '/parks', params: { :name => '', :state => 'Alabama'}
+#     binding.pry
+#     expect(response).to raise_error(Errors::StateNameNotFound)
+#   end
+#   it 'returns an exception if state is blank' do
+#     post '/parks', params: { :name => 'Da Best Park III', :state => ''}
+#     expect(response).to raise_error(Errors::StateNameNotFound)
+#   end
+#   it 'returns an exception if both name and state is blank' do
+#     post '/parks', params: { :name => '', :state => ''}
+#     expect(response).to raise_error(Errors::StateNameNotFound)
+#   end
+# end
