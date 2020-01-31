72de4a48e67add46ef4eb81e94c58a7e7b05d6be
Peter Wells <petethebeatwells@gmail.com>
2020-01-31 09:59:35 -0800

    update controllers to return better feedback to user

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,12 +1,40 @@
 class ParksController < ApplicationController
+  include Response
 
   def index
-    @parks = {"quotation": "The secret of getting ahead is getting started."}
+    @parks = Park.all
     json_response(@parks)
   end
 
-  private
-  def json_response(object, status = :ok)
-    render json: object, status: status
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    json_response(@park. :created)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "This park has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    if @park.destroy
+      render status: 200, json: {
+        message: "This park has been destroyed successfully."
+      }
+    end
+  end
+
+  def park_params
+    params.permit(:state, :name, :national)
   end
 end
